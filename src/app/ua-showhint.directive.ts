59879a00db59ce2fb14c49e1a5bbd7ce3b7b750e
daxtersky <daxtersky@gmail.com>
2020-08-14 10:47:35 +0200

    code refactor

--- src/app/ua-showhint.directive.ts
+++ src/app/ua-showhint.directive.ts
@@ -4,13 +4,9 @@ import { Directive, ElementRef, Input, HostListener, Renderer2 } from '@angular/
   selector: '[uaShowHint]'
 })
 export class UaShowHintDirective {
-  @Input('uaShowHint') defaultColor: string;
-  hoverColor: string;
-
   constructor(private elem: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    // console.log('HOVER!!', this.elem.nativeElement);
     if (this.elem.nativeElement.scrollWidth > this.elem.nativeElement.clientWidth) {
       this.showTooltip();
     }
@@ -23,5 +19,3 @@ export class UaShowHintDirective {
   private showTooltip = () => this.renderer.setAttribute(this.elem.nativeElement, 'title', this.elem.nativeElement.innerHTML);
   private hideTooltip = () => this.renderer.removeAttribute(this.elem.nativeElement, 'title');
 }
-
-// style="cursor: default; width: 170px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"
